2faef071a15fe47f2d8245f861d6d87a99f4ab04
anderson <anderdavid86@hotmail.com>
2019-11-27 11:33:34 -0500

    Se agrega toolbar con boton configuracion

--- app/src/main/java/com/smartappsolutions/turnera/LoginActivity.kt
+++ app/src/main/java/com/smartappsolutions/turnera/LoginActivity.kt
@@ -2,11 +2,56 @@ package com.smartappsolutions.turnera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class LoginActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_login)
+        setSupportActionBar(toolbar)
+
+        /*val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout_login)*/
+        /*val navView: NavigationView = findViewById(R.id.nav_view)*/
+
+       /* val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )*/
+       /* drawerLayout.addDrawerListener(toggle)*/
+       /* toggle.syncState()*/
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_login, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> settings()
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun settings(): Boolean {
+        Toast.makeText(applicationContext,"settings",Toast.LENGTH_LONG).show()
+        return true
     }
 }
+
+
--- app/src/main/java/com/smartappsolutions/turnera/MainActivity.kt
+++ app/src/main/java/com/smartappsolutions/turnera/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
