5ee846f134e99fd80914f2d3c8548beec456496a
anderson <anderdavid86@hotmail.com>
2020-01-03 12:17:43 -0500

    Se implementa statusbar en  activity asuntos

--- app/src/main/java/com/smartappsolutions/turnera/view/AsuntosAcitivity.kt
+++ app/src/main/java/com/smartappsolutions/turnera/view/AsuntosAcitivity.kt
@@ -3,7 +3,12 @@ package com.smartappsolutions.turnera.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import com.smartappsolutions.turnera.R
+import com.smartappsolutions.turnera.view.dialogs.MDialogSettings
 
 class AsuntosAcitivity : AppCompatActivity() {
 
@@ -13,5 +18,32 @@ class AsuntosAcitivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_asuntos)
         Log.d(TAG,"onCreate()")
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_asuntos)
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_asuntos, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_logOut -> logOut()
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun logOut(): Boolean {
+        Toast.makeText(applicationContext,"LogOut",Toast.LENGTH_LONG).show()
+
+        return true
     }
 }
